00980f33b7e3152c06d9ec76fbdfa8c6b57b78cd
Dustin Joynt <dustinjoynt@gmail.com>
2022-12-19 22:49:55 -0700

    Build Matrix of Trees
    - parse input into matrix

--- day8/day8.go
+++ day8/day8.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"advent-of-code-2022/input"
+	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 )
+
+type Tree struct {
+}
 
 func main() {
 
@@ -12,4 +18,30 @@ func main() {
 		log.Fatalf("failed to get input for day eight: %v", err)
 	}
 
+	trees := buildMap(input)
+	fmt.Println(trees)
+}
+
+func buildMap(input string) [][]int {
+
+	rowRe := regexp.MustCompile(`\n`)
+	rows := rowRe.Split(input, -1)
+	rows = rows[:len(rows)-1]
+
+	trees := make([][]int, len(rows))
+
+	for i, row := range rows {
+		columnRe := regexp.MustCompile(``)
+		columns := columnRe.Split(row, -1)
+
+		intColumn := []int{}
+		for _, column := range columns {
+			i, _ := strconv.Atoi(column)
+			intColumn = append(intColumn, i)
+		}
+		// trees[i] = make([]int, len(columns[i]))
+		trees[i] = intColumn
+	}
+
+	return trees
 }
